d8c7c412f1ee8ae861273e88e46e05fde332e1f8
zerihun-techin <zkebede7@gmail.com>
2020-01-29 18:06:23 +0300

    trial 3

--- DjangoTechin/mysite/polls/models.py
+++ DjangoTechin/mysite/polls/models.py
@@ -13,3 +13,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    
+class Trial(models.Model):
+    name = models.TextField()
